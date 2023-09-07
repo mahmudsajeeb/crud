9bcfa780d86c426adf265a57a474cff498487932
Mahmud sajib <mahmudsajib082@gmail.com>
2023-09-07 23:05:20 +0600

    login sign

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Login from './Login.jsx';
+import SignIn from './Sigin.jsx';
+import AuthProviders from './providers/AuthProviders.jsx';
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  
+     <AuthProviders>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </Router>
+    </AuthProviders>
   </React.StrictMode>,
 )
